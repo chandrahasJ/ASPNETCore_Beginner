b7044f5832c635b04df3a7efbcc47ee5634c9d0b
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2018-09-13 16:04:11 +0530

    Dotnet Command Line and Some Stuff

---
+++ CoreNetCLIAndTemplating/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoreNetCLIAndTemplating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = "CJ";
+            var j = 7;
+            Console.WriteLine("Hello CJ!");
+            Console.ReadLine();           
+        }
+    }
+}
