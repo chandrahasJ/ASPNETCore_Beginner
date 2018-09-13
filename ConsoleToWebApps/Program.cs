9c2c2cc9b72dbe6b732ec38659ed1b90e53c1231
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2018-09-13 18:03:13 +0530

    Console To Web Apps
    Console To Web Apps Part 1

---
+++ ConsoleToWebApps/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleToWebApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's Check");
+            //CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
