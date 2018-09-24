1b00fb48e7c3cd1f892ea15c35b510a85216ece5
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2018-09-24 21:59:17 +0530

    Create Read Update & Delete
    Commit - Create

---
+++ CRUD/RazorPagesWithCRUD/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesWithCRUD
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) 
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Cutomers { get; set; }
+    }
+}
---
+++ CRUD/RazorPagesWithCRUD/Pages/Create.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesWithCRUD.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext db;
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public CreateModel(AppDbContext _db)
+        {
+            db = _db;
+        }
+        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            db.Cutomers.Add(Customer);
+            await db.SaveChangesAsync();
+            return RedirectToPage("/");
+        }
+    }
+}
