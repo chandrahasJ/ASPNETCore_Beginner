a506434ec086f2f84e2e87a5a06d259c10f19811
chandrahasJ <38713303+chandrahasJ@users.noreply.github.com>
2018-09-30 21:43:32 +0530

    Logging
    Atlast  I Completed Beginner
    Now Lets Start With Intermediate
    I am NOT Done YET

---
+++ LoggingInCore/Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public ILogger<IndexModel> logger;
+        public IndexModel(ILogger<IndexModel> _logger)
+        {
+            logger = _logger;
+        }
+        public void OnGet()
+        {
+            logger.LogCritical("OnGet Called");
+            logger.LogInformation("We are having fun with logging");
+        }
+    }
+}
